df788a62f9a94ab372b94e7d5705a7623750abbc
salahbm <salahbm.001@gmail.com>
2023-03-31 23:00:31 +0900

    RenameTheProject

--- ios/CryptoWalletTests/CryptoWalletTests.m
+++ ios/CryptoWalletTests/CryptoWalletTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface wallet2Tests : XCTestCase
+@interface CryptoWalletTests : XCTestCase
 
 @end
 
-@implementation wallet2Tests
+@implementation CryptoWalletTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
